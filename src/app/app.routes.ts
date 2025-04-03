6ecbcef5c29e327c346513686f2d1f7aed4f8b06
piotrsioch <93688242+piotrsioch@users.noreply.github.com>
2025-04-03 11:23:38 +0200

    Feature/auth (#1)
    * Add login component
    
    * Add boilerplate for register component
    
    * Add register and home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/login/login.component';
+import { RegisterComponent } from './features/register/register.component';
+import { HomeComponent } from './features/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+];
