fa544bd420b7cf01281597e55eaa1415e700a216
Piotr Sioch <windyswire@gmail.com>
2025-04-07 16:55:51 +0200

    Implement edit, add and delete

--- src/app/features/categories/categories.component.ts
+++ src/app/features/categories/categories.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, OnInit, signal, ViewChild } from '@angular/core';
 import { TableComponent } from '../../shared/components/table/table.component';
 import { CategoriesApiService, Category } from '../../core/api/categories';
 import { CustomDatasource, PageChangeEvent, TableColumn } from '../../shared/components/table/table.assets';
@@ -8,6 +8,11 @@ import { ConfirmModalComponent } from '../../shared/components/modal/confirm-mod
 import { CategoriesModalComponent } from './categories-modal/categories-modal/categories-modal.component';
 import { CategoriesModalMode } from './categories-modal/categories-modal/categories-modal.model';
 
+export interface PaginationData {
+  page: number;
+  limit: number;
+}
+
 @Component({
   selector: 'tickets-categories',
   imports: [TableComponent],
@@ -15,6 +20,7 @@ import { CategoriesModalMode } from './categories-modal/categories-modal/categor
   styleUrl: './categories.component.scss',
 })
 export class CategoriesComponent implements OnInit {
+  @ViewChild(TableComponent) tableComponent!: TableComponent<Category>;
   modal = inject(ModalService);
   categoriesApiService = inject(CategoriesApiService);
   categoriesColumns = signal<TableColumn[]>([
@@ -37,6 +43,7 @@ export class CategoriesComponent implements OnInit {
   ]);
   categories = signal<CustomDatasource<Category> | null>(null);
   search = signal<string>('');
+  currentPaginationData = signal<PaginationData>({ page: 0, limit: 10 });
 
   ngOnInit(): void {
     this.loadCategories();
@@ -45,33 +52,71 @@ export class CategoriesComponent implements OnInit {
   async onSearchChanged(search: string): Promise<void> {
     this.search.set(search);
     await this.loadCategories({ search, page: 0, limit: 10 });
-
-    console.log(this.categories());
   }
 
   async onPageChanged(data: PageChangeEvent) {
+    console.log('CHANGED');
+    console.log(data);
     const search = this.search();
     const { pageIndex: page, pageSize: limit } = data;
+
+    this.currentPaginationData.set({ page, limit });
 
     await this.loadCategories({ page, limit, search });
   }
 
   async onEditClicked(category: Category): Promise<void> {
-    console.log('Edited: ', category.id);
     const updatedCategory = await this.modal.open(CategoriesModalComponent, {
       data: { category, mode: CategoriesModalMode.EDIT },
     });
-    console.log(updatedCategory);
+
+    if (!updatedCategory) {
+      return;
+    }
+
+    const categories = this.categories();
+    const newCategories = categories!.data.map(cat => (cat.id === updatedCategory.id ? updatedCategory : cat));
+
+    this.categories.set({ data: newCategories, total: categories!.total });
   }
 
-  async onDeleteClicked(_category: Category) {
-    const data = await this.modal.open(ConfirmModalComponent, { style: ModalStyle.ConfirmModal });
-    console.log(data);
+  async onDeleteClicked(category: Category) {
+    const wasDeleted = await this.modal.open(ConfirmModalComponent, { style: ModalStyle.ConfirmModal });
+    if (!wasDeleted) {
+      return;
+    }
+
+    const { page, limit } = this.currentPaginationData();
+    const search = this.search();
+
+    await this.categoriesApiService.deleteCategory(category.id);
+
+    if (this.categories()!.data.length === 1) {
+      console.log(1);
+      this.tableComponent.pageChanged.emit({
+        pageIndex: Math.max(page - 1, 0),
+        pageSize: limit,
+      });
+    }
+
+    await this.loadCategories({ search, page, limit });
   }
 
   async onAddClicked() {
-    const category = await this.modal.open(CategoriesModalComponent, { data: { mode: CategoriesModalMode.CREATE } });
-    console.log(category);
+    const newCategory = await this.modal.open(CategoriesModalComponent, { data: { mode: CategoriesModalMode.CREATE } });
+
+    if (newCategory) {
+      const categories = this.categories();
+      const currentPaginationData = this.currentPaginationData();
+      const newTotal = categories!.total + 1;
+
+      if (categories!.data.length < currentPaginationData.limit) {
+        const newCategories = [...categories!.data, newCategory];
+        this.categories.set({ data: newCategories, total: newTotal });
+      } else {
+        this.categories.set({ data: categories!.data, total: newTotal });
+      }
+    }
   }
 
   private async loadCategories(options?: PaginationOptions): Promise<void> {
