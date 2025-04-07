879bed4663df993ca0ce1f7a644d095f10de213a
piotrsioch <93688242+piotrsioch@users.noreply.github.com>
2025-04-07 22:00:16 +0200

    Feature/categoris enh (#5)
    * Add add buton to table
    
    * Add categories modals
    
    * Implement edit, add and delete
    
    * Fix pagination when deleting last element
    
    * Change location of categories modal
    
    * Fix selector

--- src/app/core/api/events/events.api.service.ts
+++ src/app/core/api/events/events.api.service.ts
@@ -5,15 +5,17 @@ import { PaginationOptions, PaginationOutput } from '../../../shared/models';
 import { buildPaginationParams, withAuthHeaders } from '../../../shared/functions';
 import { firstValueFrom } from 'rxjs';
 import { Event, CreateEvent } from './types';
+import { AuthService } from '../../services/auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EventsApiService {
   http = inject(HttpClient);
+  authService = inject(AuthService);
 
   async createEvent(data: CreateEvent): Promise<Event> {
-    const headers = withAuthHeaders();
+    const headers = withAuthHeaders(this.authService);
     const event$ = this.http.post<Event>(`${environment.apiRoot}/events`, data, { ...headers });
 
     return await firstValueFrom(event$);
@@ -33,14 +35,14 @@ export class EventsApiService {
   }
 
   async updateEvent(id: number, data: Partial<Event>): Promise<Event> {
-    const headers = withAuthHeaders();
+    const headers = withAuthHeaders(this.authService);
     const event$ = this.http.put<Event>(`${environment.apiRoot}/events/${id}`, data, { ...headers });
 
     return await firstValueFrom(event$);
   }
 
   async deleteEvent(id: number): Promise<void> {
-    const headers = withAuthHeaders();
+    const headers = withAuthHeaders(this.authService);
     const event$ = this.http.delete<void>(`${environment.apiRoot}/events/${id}`, { ...headers });
 
     return await firstValueFrom(event$);
