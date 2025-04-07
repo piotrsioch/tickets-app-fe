5e73842b1ef82cbf8a04fbdcd49df08b75464ccd
piotrsioch <93688242+piotrsioch@users.noreply.github.com>
2025-04-07 12:15:28 +0200

    Feature/modal (#4)
    * Add basic modal components
    
    * Fix styles
    
    * Delete redundant test file

---
+++ src/app/shared/components/modal/confirm-modal/confirm-modal.component.ts
@@ -0,0 +1,31 @@
+import { Component, inject } from '@angular/core';
+import { ModalHeaderComponent } from '../modal-header/modal-header.component';
+import { ModalButtonsComponent } from '../modal-buttons/modal-buttons.component';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+export interface ConfirmModalData {
+  title?: string;
+  description: string;
+}
+
+@Component({
+  selector: 'tickets-confirm-modal',
+  imports: [ModalHeaderComponent, ModalButtonsComponent],
+  templateUrl: './confirm-modal.component.html',
+  styleUrl: './confirm-modal.component.scss',
+})
+export class ConfirmModalComponent {
+  data: ConfirmModalData = inject(MAT_DIALOG_DATA);
+  dialogRef = inject(MatDialogRef<ConfirmModalComponent>);
+
+  title = this?.data?.title || 'Are you sure?';
+  description = this?.data?.description;
+
+  close(wasSavedClicked?: boolean) {
+    this.dialogRef.close(wasSavedClicked);
+  }
+
+  handleButtonClicked(wasSavedClicked: boolean) {
+    wasSavedClicked ? this.close(true) : this.dialogRef.close();
+  }
+}
