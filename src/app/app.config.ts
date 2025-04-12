d4a90d58242dcb66c3262fefcbf2d9f2a80c8351
Piotr Sioch <windyswire@gmail.com>
2025-04-12 17:51:48 +0200

    Install and use polish locale

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,25 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, DEFAULT_CURRENCY_CODE, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { loadingInterceptor } from './core/interceptors/loading.interceptor';
+import { registerLocaleData } from '@angular/common';
+import localePl from '@angular/common/locales/pl';
+
+registerLocaleData(localePl, 'pl-PL');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideHttpClient(withFetch(), withInterceptors([loadingInterceptor])),
     provideRouter(routes),
+    {
+      provide: LOCALE_ID,
+      useValue: 'pl-PL',
+    },
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'PLN',
+    },
   ],
 };
