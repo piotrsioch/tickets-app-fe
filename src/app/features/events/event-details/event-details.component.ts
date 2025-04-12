b7d236191afcae4e45645d8971fb425d5b9e646d
Piotr Sioch <windyswire@gmail.com>
2025-04-12 03:23:23 +0200

    Use socket service in event details component

--- src/app/features/events/event-details/event-details.component.ts
+++ src/app/features/events/event-details/event-details.component.ts
@@ -1,7 +1,10 @@
-import { Component, inject, signal, OnInit } from '@angular/core';
+import { Component, inject, signal, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { EventModel } from '../../../core/api/events';
 import { CurrencyPipe, DatePipe } from '@angular/common';
+import { SocketService } from '../../../core/services/tickets-socket.service';
+import { toObservable } from '@angular/core/rxjs-interop';
+import { filter, Subscription } from 'rxjs';
 
 @Component({
   selector: 'tickets-event-details',
@@ -9,12 +12,38 @@ import { CurrencyPipe, DatePipe } from '@angular/common';
   templateUrl: './event-details.component.html',
   styleUrl: './event-details.component.scss',
 })
-export class EventDetailsComponent implements OnInit {
-  event = signal<EventModel | null>(null);
-  route = inject(ActivatedRoute);
+export class EventDetailsComponent implements OnDestroy {
+  private subscription = new Subscription();
 
-  ngOnInit() {
+  route = inject(ActivatedRoute);
+  ticketSocketService = inject(SocketService);
+  event = signal<EventModel | null>(null);
+  updatedTicketAvailability = this.ticketSocketService.updatedTicketAvailability;
+
+  constructor() {
     this.event.set(this.route.snapshot.data['event']);
-    console.log(this.event());
+    this.setupTicketUpdateSubscription();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  private setupTicketUpdateSubscription() {
+    const ticketUpdate$ = toObservable(this.updatedTicketAvailability).pipe(
+      filter((update): update is { eventId: number; availableTickets: number } => !!update)
+    );
+
+    const ticketSub = ticketUpdate$.subscribe(data => {
+      const { eventId, availableTickets } = data;
+      const event = this.event();
+
+      if (event && eventId === event.id) {
+        const { availableTickets: _, ...rest } = event;
+        this.event.set({ availableTickets, ...rest });
+      }
+    });
+
+    this.subscription.add(ticketSub);
   }
 }
